59a938d1af15d97c02a40f35b98586e4a75c7284
Evelyn França <evelynsfrancaa@gmail.com>
2022-01-24 18:55:17 -0300

    Back-end Repository - refactor

--- back/src/main/java/br/com/pipo/repository/PartnerRepository.java
+++ back/src/main/java/br/com/pipo/repository/PartnerRepository.java
@@ -3,5 +3,7 @@ package br.com.pipo.repository;
 import br.com.pipo.model.Partner;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface PartnerRepository extends MongoRepository<Partner, String> {
 }
--- back/src/main/java/br/com/pipo/repository/ProductRepository.java
+++ back/src/main/java/br/com/pipo/repository/ProductRepository.java
@@ -2,6 +2,12 @@ package br.com.pipo.repository;
 
 import br.com.pipo.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends MongoRepository<Product, String> {
+
+    @Query("{'clients' :{'$ref' : 'clients' , '$id' : ?0}}")
+    List<Product> findAllByClientId(String clientId);
 }
